f6d469eeb7e4219284a047139642738ba741bac1
b-boechat <bernardoant@poli.ufrj.br>
2023-02-18 16:38:08 -0300

    Prelim. model prediction visualization.

--- test_saved_model.py
+++ test_saved_model.py
@@ -5,6 +5,7 @@ import glob
 import librosa
 import matplotlib.pyplot as plt
 from librosa.display import specshow
+from definitions import *
 
 from model import *
 from deserialize_guitarset import prepare_dataset
@@ -24,18 +25,42 @@ def restore_model(saved_model_path, label_smoothing=0.2, onset_positive_weight=0
 
     return model
 
-def print_examples(saved_model_path):
+def plot_examples(saved_model_path):
 
     model = restore_model(saved_model_path)
     data_path_list = glob.glob(r"guitarset_processed/training/" + r"/*.tfrecord")
     dataset = prepare_dataset(data_path_list, buffer_size=1, batch_size=1)
 
     for example in dataset.take(1):
-        X_contours, X_notes, X_onsets = example[1]["X_contours"][0], example[1]["X_notes"][0], example[1]["X_onsets"][0]
-        print(X_contours)
+        X_contours, X_notes, X_onsets = example[1]["X_contours"][0].numpy().transpose(), example[1]["X_notes"][0].numpy().transpose(), example[1]["X_onsets"][0].numpy().transpose()
+        pred = model.predict(example)
+        
+        pred_X_contours, pred_X_notes, pred_X_onsets = pred["X_contours"][0].transpose(), pred["X_notes"][0].transpose(), pred["X_onsets"][0].transpose()
+
+        plt.figure()
+        librosa.display.specshow(X_contours, sr=AUDIO_SAMPLE_RATE, x_axis='time', y_axis='cqt_hz',
+            hop_length=CQT_HOP_LENGTH, fmin=MINIMUM_ANNOTATION_FREQUENCY, bins_per_octave=CONTOURS_BINS_PER_OCTAVE, tuning=0.0)
+        plt.figure()
+        librosa.display.specshow(pred_X_contours, sr=AUDIO_SAMPLE_RATE, x_axis='time', y_axis='cqt_hz',
+            hop_length=CQT_HOP_LENGTH, fmin=MINIMUM_ANNOTATION_FREQUENCY, bins_per_octave=CONTOURS_BINS_PER_OCTAVE, tuning=0.0)
+
+        plt.figure()
+        librosa.display.specshow(X_notes, sr=AUDIO_SAMPLE_RATE, x_axis='time', y_axis='cqt_hz',
+            hop_length=CQT_HOP_LENGTH, fmin=MINIMUM_ANNOTATION_FREQUENCY, bins_per_octave=CONTOURS_BINS_PER_OCTAVE, tuning=0.0)
+        plt.figure()
+        librosa.display.specshow(pred_X_notes, sr=AUDIO_SAMPLE_RATE, x_axis='time', y_axis='cqt_hz',
+            hop_length=CQT_HOP_LENGTH, fmin=MINIMUM_ANNOTATION_FREQUENCY, bins_per_octave=CONTOURS_BINS_PER_OCTAVE, tuning=0.0)
+
+        plt.figure()
+        librosa.display.specshow(X_onsets, sr=AUDIO_SAMPLE_RATE, x_axis='time', y_axis='cqt_hz',
+            hop_length=CQT_HOP_LENGTH, fmin=MINIMUM_ANNOTATION_FREQUENCY, bins_per_octave=CONTOURS_BINS_PER_OCTAVE, tuning=0.0)
+        plt.figure()
+        librosa.display.specshow(pred_X_onsets, sr=AUDIO_SAMPLE_RATE, x_axis='time', y_axis='cqt_hz',
+            hop_length=CQT_HOP_LENGTH, fmin=MINIMUM_ANNOTATION_FREQUENCY, bins_per_octave=CONTOURS_BINS_PER_OCTAVE, tuning=0.0)
+        plt.show()
 
 
 if __name__ == "__main__":
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
     #restore_model("saved_models/trained.h5")
-    print_examples("saved_models/trained.h5")
+    plot_examples("saved_models/trained_lr_e3.h5")
